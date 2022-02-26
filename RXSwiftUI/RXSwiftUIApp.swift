3a36debbe686332e4a73c2dae741db145dd65980
Edvin <edvinlel@gmail.com>
2022-02-26 00:47:52 -0800

    Initial Commit

---
+++ RXSwiftUI/RXSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RXSwiftUIApp.swift
+//  RXSwiftUI
+//
+//  Created by Edvin Lellhame on 2/26/22.
+//
+
+import SwiftUI
+
+@main
+struct RXSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
